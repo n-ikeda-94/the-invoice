6c35d793002fea4d504ab48aaf413bc76c3401f1
n-ikeda-94 <59464339+n-ikeda-94@users.noreply.github.com>
2020-04-01 22:42:36 +0900

    migrationFile(statement)

--- db/migrate/20200330115525_create_statements.rb
+++ db/migrate/20200330115525_create_statements.rb
@@ -1,7 +1,15 @@
 class CreateStatements < ActiveRecord::Migration[5.0]
   def change
     create_table :statements do |t|
-
+      t.integer :invoice_id
+      t.integer :number
+      t.integer :event_id
+      t.string :text
+      t.integer :unit_price
+      t.integer :quantity
+      t.integer :tax_rate
+      t.integer :sales_tax
+      t.integer :sub_total
       t.timestamps
     end
   end

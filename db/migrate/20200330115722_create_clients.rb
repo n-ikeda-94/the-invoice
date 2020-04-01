db3c8c747c9d5a38ba16c975fddbe37b6296c03f
n-ikeda-94 <59464339+n-ikeda-94@users.noreply.github.com>
2020-04-01 22:47:01 +0900

    migrationFile(client)

--- db/migrate/20200330115722_create_clients.rb
+++ db/migrate/20200330115722_create_clients.rb
@@ -1,7 +1,10 @@
 class CreateClients < ActiveRecord::Migration[5.0]
   def change
     create_table :clients do |t|
-
+      t.string :name
+      t.string :postcode
+      t.string :address
+      t.integer :company_id
       t.timestamps
     end
   end

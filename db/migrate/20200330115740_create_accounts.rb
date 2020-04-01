b0d1a50b8f96898d82534bae60e18bc25d487acf
n-ikeda-94 <59464339+n-ikeda-94@users.noreply.github.com>
2020-04-01 22:48:33 +0900

    migrationFile(account)

--- db/migrate/20200330115740_create_accounts.rb
+++ db/migrate/20200330115740_create_accounts.rb
@@ -1,7 +1,8 @@
 class CreateAccounts < ActiveRecord::Migration[5.0]
   def change
     create_table :accounts do |t|
-
+      t.string :name
+      t.integer :attribution
       t.timestamps
     end
   end

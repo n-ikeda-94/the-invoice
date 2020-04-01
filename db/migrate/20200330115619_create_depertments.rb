6b8ea02c5422799b5e5a82c0e5af2e2dc138a59d
n-ikeda-94 <59464339+n-ikeda-94@users.noreply.github.com>
2020-04-01 22:44:43 +0900

    migrationFile(depertment)

--- db/migrate/20200330115619_create_depertments.rb
+++ db/migrate/20200330115619_create_depertments.rb
@@ -1,7 +1,8 @@
 class CreateDepertments < ActiveRecord::Migration[5.0]
   def change
     create_table :depertments do |t|
-
+      t.string :name
+      t.integer :company_id
       t.timestamps
     end
   end

a030303ae0e8b7fedbd1670ea42d9f8efc5d8dba
n-ikeda-94 <59464339+n-ikeda-94@users.noreply.github.com>
2020-04-02 22:45:09 +0900

    routes_initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :companies, only: [:new, :create]
+  resources :invoices, only: [:index, :new, :create]
+  resources :statements, only: [:new, :create]
+  resources :departments, only: [:new, :create]
+  resources :clients, only: [:new, :create]
 end
